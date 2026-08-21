11a5c5f908d62a8978ea178814f5a7cd2760e2e5
Patricia Braga <115669838+PatiBraga@users.noreply.github.com>
2026-08-21 11:39:17 -0300

    Update update de sala na itped_rx.sql

--- Tabelas MV/T - Diagnostico/update de sala na itped_rx.sql
+++ Tabelas MV/T - Diagnostico/update de sala na itped_rx.sql
@@ -21,3 +21,25 @@ AND cd_exa_rx = 1137;
 ALTER TRIGGER mvintegra.trg_imvw_sai_itsolic_psdi ENABLE;
 /
 
+-- Validar o status da trigger pela view ALL_TRIGGERS:
+ SELECT
+    OWNER,
+    TRIGGER_NAME,
+    TABLE_OWNER,
+    TABLE_NAME,
+    STATUS,
+    TRIGGERING_EVENT,
+    TRIGGER_TYPE
+FROM ALL_TRIGGERS
+WHERE OWNER = 'MVINTEGRA'
+  AND TRIGGER_NAME = 'TRG_IMVW_SAI_ITSOLIC_PSDI';
+/
+
+-- Caso esteja conectado com o proprietário da trigger (MVINTEGRA), também pode utilizar:
+SELECT
+    TRIGGER_NAME,
+    TABLE_NAME,
+    STATUS
+FROM USER_TRIGGERS
+WHERE TRIGGER_NAME = 'TRG_IMVW_SAI_ITSOLIC_PSDI';
+
