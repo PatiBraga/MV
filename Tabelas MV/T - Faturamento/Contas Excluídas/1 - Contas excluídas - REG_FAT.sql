5140ef282138538234dbac087fea1359f9d16dc9
Patricia Braga <115669838+PatiBraga@users.noreply.github.com>
2026-08-25 13:26:16 -0300

    Update SQL query for excluded accounts validation
    Added a comment for validation and retained the original SQL query structure.

--- Tabelas MV/T - Faturamento/Contas Excluídas/1 - Contas excluídas - REG_FAT.sql
+++ Tabelas MV/T - Faturamento/Contas Excluídas/1 - Contas excluídas - REG_FAT.sql
@@ -1,12 +1,13 @@
+-- Validar Auditória - Contas excluídas - REG_FAT
 SELECT
-    a.cd_reg_fat AS "C�digo da Conta",
-    a.cd_atendimento AS "C�digo do Atendimento",
-    a.cd_convenio AS "C�digo do Conv�nio",
+    a.cd_reg_fat AS "Código da Conta",
+    a.cd_atendimento AS "Código do Atendimento",
+    a.cd_convenio AS "Código do Convênio",
 
     CASE
         WHEN a.cd_remessa IS NULL THEN 'SEM REMESSA'
         ELSE TO_CHAR(a.cd_remessa)
-    END AS "C�digo da Remessa",
+    END AS "Código da Remessa",
 
     CASE
         WHEN a.vl_total_conta IS NULL THEN 'SEM VALOR'
@@ -14,17 +15,17 @@ SELECT
     END AS "Valor da Conta",
 
     a.audit_dt_registro AS "Aud. Data Registro",
-    a.audit_cd_usuario AS "Aud. Usu�rio",
+    a.audit_cd_usuario AS "Aud. Usuário",
     a.sn_fechada AS "Fechada",
 
     CASE
-        WHEN a.audit_tp_acao = 'E' THEN 'Exclu�do'
+        WHEN a.audit_tp_acao = 'E' THEN 'Excluído'
         ELSE a.audit_tp_acao
-    END AS "Tipo da A��o"
+    END AS "Tipo da Ação"
 
 FROM audit_dbamv.reg_fat a
 WHERE a.audit_tp_acao = 'E'
-  AND a.audit_dt_registro >= DATE '2026-01-01'
-  AND a.audit_dt_registro < DATE '2026-09-01'
--- AND a.cd_reg_fat = 115813
+        AND a.audit_dt_registro >= DATE '2026-01-01'
+        AND a.audit_dt_registro < DATE '2026-09-01'
+        -- AND a.cd_reg_fat = 115813
 ORDER BY a.audit_dt_registro;
