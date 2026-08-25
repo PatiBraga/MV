86b3d41ba04fea8b49b9212565d39eb6a61aadc2
Patricia Braga <115669838+PatiBraga@users.noreply.github.com>
2026-08-25 13:21:52 -0300

    Rename Contas excluídas - REG_AMB.sql to 1 - Contas excluídas - REG_AMB.sql

--- Tabelas MV/T - Faturamento/Contas Excluídas/1 - Contas excluídas - REG_AMB.sql
+++ Tabelas MV/T - Faturamento/Contas Excluídas/1 - Contas excluídas - REG_AMB.sql
@@ -1,14 +1,14 @@
 -- Validar AUDITORIA - Tabela REG_AMB
 
 SELECT
-    a.cd_reg_amb AS "Código da Conta Ambulatorial",
-    a.cd_convenio AS "Código do Convênio",
-    a.cd_remessa AS "Código da Remessa",
+    a.cd_reg_amb AS "CÃ³digo da Conta Ambulatorial",
+    a.cd_convenio AS "CÃ³digo do ConvÃªnio",
+    a.cd_remessa AS "CÃ³digo da Remessa",
     a.vl_total_conta AS "Valor Total da Conta",
-    a.dt_lancamento AS "Data de Lançamento",
+    a.dt_lancamento AS "Data de LanÃ§amento",
     a.audit_dt_registro AS "Data do Registro de Auditoria",
-    a.audit_cd_usuario AS "Usuário da Auditoria",
-    a.audit_tp_acao AS "Tipo da Ação",
+    a.audit_cd_usuario AS "UsuÃ¡rio da Auditoria",
+    a.audit_tp_acao AS "Tipo da AÃ§Ã£o",
     a.sn_fechada AS "Conta Fechada"
 FROM audit_dbamv.reg_amb a
 WHERE a.audit_dt_registro >= DATE '2022-01-01'
